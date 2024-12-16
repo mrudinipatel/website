d3c63a5bb1e9027be150536689d4eeb729652b1d
mrudinipatel <mrudinipatel@gmail.com>
2024-12-15 21:32:47 -0500

    need to use hex code for heart in footer

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from './FooterStyles.module.css';
 function Footer() {
   return (
     <section className={styles.container} id="footer">
-        <p>&copy; 2024 Made with ❤ by Mrudini </p>
+        <p>&copy; 2024 Made with &#x2764; by Mrudini </p>
     </section>
   );
 }
